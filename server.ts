dbb714907f31e93b60a00389c384b9d6b0600cfa
unknown <jakub.gencer@allaboutapps.at>
2019-08-30 19:38:11 +0200

    Added await function for mongodb connect

--- server.ts
+++ server.ts
@@ -7,17 +7,27 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: false}));
 
-
-const connect = async () => {
+const startServer = () => {
     app.listen(port, async (req, res) => {
         console.log("REQUEST -> "+req+" /RESULT -> "+res);
     });
 }
+mongoose.connect('127.0.0.1:27017/api')
+    .then(() => {
+        console.log("Test");
+        startServer()
+    })
+    .then(() => console.log("Test"))
+    .catch(Error);
+
+
+
+
+
 const get = async () => {
     await app.route('/')
         .get(async (req, res) => {
-            
+
         });
 }
-connect();
 get()
